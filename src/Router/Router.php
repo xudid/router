2e06c34c1f0575488d9eb76ead345ae6a5c3de1b
Didier Moindreau <Didux74@gmail.com>
2019-12-25 18:49:26 +0100

    separate middleware behavior from Router class

--- src/Router/Router.php
+++ src/Router/Router.php
@@ -3,64 +3,51 @@ namespace Router;
 
 
 use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Server\MiddlewareInterface;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Server\RequestHandlerInterface;
-
 
 /**
-  * Router register Route(s) and parse them
-  * with method run
-  */
-class Router implements MiddlewareInterface
-{
-  /**
-   * @var string $url
-   */
-  private $url=null;
-
-  private $authorizedMethods = [];
-  /**
-   * @var array $routes
-   */
-  private $routes= array();
-
-/**
- * @var bool success
+ * Router register Route(s) and parse them
+ * with method run
  */
- private $success = false;
+class Router
+{
+    /**
+     * @var string $url
+     */
+    private $url = null;
 
- /**
- * Route
-  * @var  $currentRoute;
-  */
-  private $currentRoute;
+    private $authorizedMethods = [];
+    /**
+     * @var array $routes
+     */
+    private $routes = array();
 
-  private function addRoute(
-                             string $method,
-                             string $scope,
-                             string $path,
-                             string $name,
-                             $callable
-                           ) :Route
-  {
-      if(in_array($method, $this->authorizedMethods))
-      {
-          $route = new Route($path,$name, $callable);
-          $this->routes[$method][$scope][$name]=$route;
-          return $route;
-      } else {
-          throw new RouterException("Try to add a route with an unauthorized method");
-      }
+    /**
+     * @param string $method
+     * @param string $scope
+     * @param string $path
+     * @param string $name
+     * @param $callable
+     * @return Route
+     * @throws RouterException
+     */
+    private function addRoute(
+        string $method,
+        string $scope,
+        string $path,
+        string $name,
+        $callable
+    ): Route
+    {
+        if (in_array($method, $this->authorizedMethods)) {
+            $route = new Route($path, $name, $callable);
+            $this->routes[$method][$scope][$name] = $route;
+            return $route;
+        } else {
+            throw new RouterException("Try to add a route with an unauthorized method");
+        }
 
-  }
-  /**
-   *
-   */
-  function __construct()
-  {
+    }
 
-  }
     /**
      * Register a Route to a ressource asked with a HTTP POST method
      * @param string $scope the logic url domain without a '/'
@@ -71,144 +58,139 @@ class Router implements MiddlewareInterface
      * on this object , the callback function to call after that
      * @return Route
      *
+     * @throws RouterException
      */
-  public function get(
-                        string $scope,
-                        string $path,
-                        string $name,
-                        $callable
-                    ):Route
- {
+    public function get(
+        string $scope,
+        string $path,
+        string $name,
+        $callable
+    ): Route
+    {
 
-    return $this->addRoute("GET", $scope, $path, $name, $callable);
+        return $this->addRoute("GET", $scope, $path, $name, $callable);
 
- }
-/**
- * Register a Route to a ressource asked with a HTTP POST method
- * @param string $scope the logic url domain without a '/'
- * @param string $path the actual url path to the ressource with beginning '/'
- * @param string|null $name string to display in menu
- * @param array|callable $callable callback function to call if route match
- * the array contains in order an object , the name of a function to call
- * on this object , the callback function to call after that
- * @return Route
- *
- */
-  public function post(
-                        string $scope,
-                        string $path,
-                        string $name,
-                        $callable
-                    ):Route
- {
+    }
 
-     return $this->addRoute("POST", $scope, $path, $name, $callable);
+    /**
+     * Register a Route to a ressource asked with a HTTP POST method
+     * @param string $scope the logic url domain without a '/'
+     * @param string $path the actual url path to the ressource with beginning '/'
+     * @param string|null $name string to display in menu
+     * @param array|callable $callable callback function to call if route match
+     * the array contains in order an object , the name of a function to call
+     * on this object , the callback function to call after that
+     * @return Route
+     *
+     * @throws RouterException
+     */
+    public function post(
+        string $scope,
+        string $path,
+        string $name,
+        $callable
+    ): Route
+    {
 
- }
+        return $this->addRoute("POST", $scope, $path, $name, $callable);
 
+    }
+
+    /**
+     * @param string $scope
+     * @param string $path
+     * @param string $name
+     * @param $callable
+     * @return Route
+     * @throws RouterException
+     */
     public function put(
         string $scope,
         string $path,
         string $name,
         $callable
-    ):Route
+    ): Route
     {
 
         return $this->addRoute("PUT", $scope, $path, $name, $callable);
 
     }
 
+    /**
+     * @param string $scope
+     * @param string $path
+     * @param string $name
+     * @param $callable
+     * @return Route
+     * @throws RouterException
+     */
     public function delete(
         string $scope,
         string $path,
         string $name,
         $callable
-    ):Route
+    ): Route
     {
 
         return $this->addRoute("DELETE", $scope, $path, $name, $callable);
 
     }
 
+    /**
+     * @param string $scope
+     * @param string $path
+     * @param string $name
+     * @param $callable
+     * @return Route
+     * @throws RouterException
+     */
     public function options(
         string $scope,
         string $path,
         string $name,
         $callable
-    ):Route
+    ): Route
     {
 
         return $this->addRoute("OPTIONS", $scope, $path, $name, $callable);
 
     }
 
-    /**
-     * Run registred routes exploration
-     * If a route match put success attribute at true on the request
-     * Set the route attribut on the request and the returnAppPage to true
-     * on it .
-     * Else set success attribute at false on the request
-     * Return both the handled response
-     * @param ServerRequestInterface $request
-     * @param RequestHandlerInterface $handler the fianl response handler
-     * @return ResponseInterface
-     */
-  public function process(ServerRequestInterface $serverrequest, RequestHandlerInterface $handler): ResponseInterface
-  {
-    $response = $handler->handle($serverrequest);
-    $this->url = ($serverrequest->getUri()->getPath());
-    $parts = \explode('/',$this->url);
-    $scope = $parts[0];
-    if(empty($scope)){$scope = "default";}
-
-    $method = $serverrequest->getMethod();
-
-    //Enigmatic request
-    if(!isset($this->routes[$method][$scope]))
+    public function match(ServerRequestInterface $request): ?Route
     {
-      $handler->handle($serverrequest
-      ->withAttribute("success" , $this->success));
-      return $response;
-    }
-
-    //Walk through the routes
-    foreach ($this->routes[$method][$scope] as $name =>$route)
-    {
-        if($route->match($serverrequest))
-        {
-          $this->success = true;
-          $this->currentRoute =$route;
-          break;
+        $this->url = ($request->getUri()->getPath());
+        $parts = explode('/', $this->url);
+        $scope = $parts[0];
+        if (empty($scope)) {
+            $scope = "default";
         }
+        $method = $request->getMethod();
+        $matchedRoute = null;
+        //Enigmatic request
+        if (!isset($this->routes[$method][$scope])) {
+            return $matchedRoute;
+        }
+
+        //Walk through the routes
+        foreach ($this->routes[$method][$scope] as $name => $route) {
+            if ($route->match($request)) {
+                $matchedRoute =  $route;
+                break;
+            }
+        }
+        return $matchedRoute;
     }
 
-    //We found the promise land
-    if($this->success)
+    /**
+     * hasParams test if the path of the route has params
+     * like ":id" return true  if has params else return false
+     * @param string $url [description]
+     * @return bool        [description]
+     */
+    private function hasParams(string $url): bool
     {
-      $handler->handle($serverrequest
-      ->withAttribute("success" , $this->success)
-      ->withAttribute("route", $this->currentRoute)
-     );
+        return preg_match('#:([\w]+)#', $url, $matches) ? true : false;
     }
-    //We are lost no route found
-    else
-    {
-        $handler->handle($serverrequest
-                         ->withAttribute("success" , $this->success));
-    }
-    return $response ;
-   }
-
-/**
- * hasParams test if the path of the route has params
- * like ":id" return true  if has params else return false
- * @param  string $url [description]
- * @return bool        [description]
- */
-   private function hasParams(string $url):bool
-   {
-       return  \preg_match('#:([\w]+)#',$url,$matches) ?true:false;
-   }
 
     /**
      * @param string $name the name of the we want to generate an url from
@@ -216,30 +198,30 @@ class Router implements MiddlewareInterface
      * @return string
      * @throw RouterException
      */
-    public function generateUrl(string $scope, string $name, array $params=[])
-   {
-       $url = "test";
-       //Does it need an isRouteExist($method,$scope,$name):bool  ?
-       if (array_key_exists('GET',$this->routes) &&
-           array_key_exists($scope,$this->routes['GET'] )&&
-           array_key_exists($name,$this->routes['GET'][$scope])) {
+    public function generateUrl(string $scope, string $name, array $params = [])
+    {
+        $url = "test";
+        //Does it need an isRouteExist($method,$scope,$name):bool  ?
+        if (array_key_exists('GET', $this->routes) &&
+            array_key_exists($scope, $this->routes['GET']) &&
+            array_key_exists($name, $this->routes['GET'][$scope])) {
 
-           $route = $this->routes['GET'][$scope][$name];
-           $path = $route->getPath();
-           $routeParams = $route->getParams();
-           foreach ($params as $key => $value) {
-               if (array_key_exists($key, $routeParams)) {
-                   $pattern = "#(:" . $key . ")#";
-                   $replacement = $params[$key];
-                   $path = preg_replace($pattern, $replacement, $path);
-               }
-           }
-           $url = '/'.$path;
-       }
+            $route = $this->routes['GET'][$scope][$name];
+            $path = $route->getPath();
+            $routeParams = $route->getParams();
+            foreach ($params as $key => $value) {
+                if (array_key_exists($key, $routeParams)) {
+                    $pattern = "#(:" . $key . ")#";
+                    $replacement = $params[$key];
+                    $path = preg_replace($pattern, $replacement, $path);
+                }
+            }
+            $url = '/' . $path;
+        }
 
-       return $url;
+        return $url;
 
-   }
+    }
 
     /**
      * @param array $authorizedMethods
