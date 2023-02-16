8963a495c48096982b66ead44bc7fb37d516f967
Didier Moindreau <dmoindreau@gmail.com>
2023-02-16 23:10:17 +0100

    RouterTest simplify callbacks

--- test/RouterTest.php
+++ test/RouterTest.php
@@ -27,28 +27,29 @@ class RouterTest extends TestCase
     {
         $this->expectException(RouterException::class);
         $router = new Router();
-        $router->get('/root', 'root', function(){echo 'hello world';});
+        $router->get('/root', 'root', fn() => '');
     }
 
     public function testGet()
     {
-        $route = $this->router->get('/root', 'root', function(){echo 'hello world';});
+        $route = $this->router->get('/root', 'root', fn() => '');
         self::assertInstanceOf(Route::class,$route);
     }
 
     public function testPost()
     {
-        $route = $this->router->post('/root', 'root', function(){echo 'hello world';});
+        $route = $this->router->post('/root', 'root', fn() => '');
         self::assertInstanceOf(Route::class,$route);
     }
 
     public function testGenerateUrl()
     {
-        $this->router->get('/simple/:id','simple_show',function(){return true;})
+        $this->router->get('/simple/:id','simple_show', fn() => '')
 			->with('id','[\d+]');
         $url = $this->router->generateUrl('simple_show', ['id' =>3]);
+
         self::assertEquals('/simple/3',$url);
-        $this->router->get('/simple1/:id/:a_id','simple_show_2',function(){return true;})
+        $this->router->get('/simple1/:id/:a_id','simple_show_2', fn() => '')
 			->with('id','[\d+]')->with('a_id','[\d+]');
         $url = $this->router->generateUrl( 'simple_show_2',['id' =>3,'a_id' =>2]);
         self::assertEquals('/simple1/3/2',$url);
